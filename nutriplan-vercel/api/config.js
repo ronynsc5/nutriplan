7f644605b2ef6ec2375012e23abf1296d90e0062
ronynsc5 <ronynsc5@gmail.com>
2026-05-04 14:52:55 -0300

    Delete nutriplan-vercel/api/config.js

--- nutriplan-vercel/api/config.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({
-    MP_PUBLIC_KEY: process.env.MP_PUBLIC_KEY || ''
-  });
-} 
