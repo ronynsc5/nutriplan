27e350ec1e98de22ba3417399e6b907b2ce5b5d5
ronynsc5 <ronynsc5@gmail.com>
2026-05-04 13:06:22 -0300

    Create config.js

---
+++ nutriplan-vercel/api/config.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({
+    MP_PUBLIC_KEY: process.env.MP_PUBLIC_KEY || ''
+  });
+}
